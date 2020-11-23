88a5adf84f11300cc3f7d26e6481af6246703ec5
gerardmilan501 <72268292+gerardmilan501@users.noreply.github.com>
2020-11-23 12:01:18 +0100

    Update Alumno.java

--- src/main/java/edu/upc/dsa/models/Alumno.java
+++ src/main/java/edu/upc/dsa/models/Alumno.java
@@ -1,4 +1,60 @@
 package edu.upc.dsa.models;
 
+import edu.upc.dsa.util.RandomUtils;
+
 public class Alumno {
+
+    String id;
+    String nombre;
+    int edad;
+    String salud;
+    static int lastId;
+
+    public Alumno() {
+        this.id = RandomUtils.getId();
+    }
+
+    public Alumno(String nombre, int edad, String salud) {
+        this();
+        this.setNombre(nombre);
+        this.setEdad(edad);
+        this.setSalud(salud);
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSalud() {
+        return salud;
+    }
+
+    public void setSalud(String salud) {
+        this.salud = salud;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno [id="+id+", nombre=" + nombre + ", edad=" + edad +", salud=" + salud "]";
+    }
 }
