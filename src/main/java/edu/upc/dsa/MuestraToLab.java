f226236cfdbe4b9196aa16109da378ab1eeccdbe
gerardmilan501 <72268292+gerardmilan501@users.noreply.github.com>
2020-11-23 12:31:30 +0100

    Update MuestraToLab.java

--- src/main/java/edu/upc/dsa/MuestraToLab.java
+++ src/main/java/edu/upc/dsa/MuestraToLab.java
@@ -29,5 +29,41 @@ public class MuestraToLab implements MuestraManager{
         return muestra;
     }
 
+    public muestra addmuestra(String idmuestra, String idclinic, String id, String fecha, String idlab) {
+        return this.addmuestra(new muestra(idmuestra, idclinic, id, fecha, idlab));
+    }
+
+    public muestra getmuestra(String idmuestra) {
+        logger.info("getmuestra(" + idmuestra + ")");
+
+        for (muestra muestra : this.muestras) {
+            if (muestra.getIdmuestra().equals(idmuestra)) {
+                logger.info("getmuestra(" + idmuestra + "): " + muestra);
+
+                return muestra;
+            }
+        }
+
+        logger.warn("not found " + idmuestra);
+        return null;
+    }
+
+    @Override
+    public muestra updatemuestra(muestra m) {
+        muestra muestra = this.getmuestra(m.getIdmuestra());
+
+        if (m.getSalud()!="A") {
+            logger.info("POSITIVO");
+
+
+            logger.info(m +" updated ");
+        }
+        else {
+            logger.warn("Negativo "+m.this.nombre);
+        }
+
+        return m;
+    }
+
 
 }
